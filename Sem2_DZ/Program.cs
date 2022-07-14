90f60e00cf98054d6f908c006b88d239474abfec
Тихомиров Сергей <s.o.tikhomirov@gmail.com>
2022-07-14 10:48:11 +0300

    Добавлено домашнее задание ко 2 семинару с 1-й дополнительной задачей

--- Sem2_DZ/Program.cs
+++ Sem2_DZ/Program.cs
@@ -28,16 +28,27 @@ void Zadacha13()
         
         Console.WriteLine("Введите число от 10 до 999");
         int number = Convert.ToInt32(Console.ReadLine());
-        int ones = number % 10;
-        int tens = number / 10;
-        int hundred = number / 100;
+        int numberAbs = Math.Abs(number);
+        int n = numberAbs;
+        int del = 10;
+        
+        while (numberAbs > del && n > 999)
+        {
+               n = numberAbs / del;
+               del = del * 10;         
+        } 
+        //Console.WriteLine(n); 
+        int ones = n % 10;
+        //int tens = n / 10;
+        int hundred = n / 100;   
         //Console.WriteLine(ones);
         //Console.WriteLine(tens - hundred * 10);
+        //Console.WriteLine(hundred);
         if (hundred == 0 )
         {
             Console.WriteLine("Третьей цифры нет");
         }
-        else Console.WriteLine(ones);
+        else Console.WriteLine("Третья цифра " + ones);//
 
 
     }
@@ -63,4 +74,38 @@ void Zadacha15()
 
 
     }
-Zadacha15();
+//Zadacha15();
+
+
+void Zadacha1() 
+
+    {
+        //Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным.
+
+        
+        Console.WriteLine("Введите длину стороны a");
+        int sideA = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите длину стороны b");
+        int sideB = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите длину стороны c");
+        int sideC = Convert.ToInt32(Console.ReadLine());
+        
+        if (sideA == sideB || sideA == sideC || sideB == sideC)
+        {
+            if (((sideA + sideB) > sideC) || ((sideA + sideC) > sideB) || ((sideC + sideB) > sideA))
+            {
+                Console.WriteLine("Треугольник равнобедренный");
+            }
+            else if ((sideA == sideB) && (sideA == sideC))
+            {
+                Console.WriteLine("Треугольник равносторонний");
+            }
+            else Console.WriteLine("Что ты такое?!");       
+
+        }
+        else Console.WriteLine("Треугольник не является равенобедренным");
+        
+        
+
+    }
+//Zadacha1();
